046a222a6dd785252e42e87355cbb9df63fb2eab
zhenghuiqing <523841743@qq.com>
2020-05-23 21:38:23 +0800

    提交测试方法需求

--- git_test/src/com/ittest/git/Gittest.java
+++ git_test/src/com/ittest/git/Gittest.java
@@ -3,5 +3,9 @@ package com.ittest.git;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("git测试");
+        tes();
+    }
+    public static void  tes(){
+        System.out.println("哈哈哈哈");
     }
 }
